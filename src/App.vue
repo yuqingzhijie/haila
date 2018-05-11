c8d0839d618984fa86b48da219b0d4db8aae7813
yuqingzhijie <2711676102@qq.com>
2018-05-11 10:13:56 +0800

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -9,34 +9,4 @@ export default {
   name: 'App'
 }
 </script>
-<template>
-  <div>
-    页面内容
-  </div>
-</template>
-
-<script>
-  import css / vue / 第三方依赖;
-
-  export default {
-    data () {
-      return { 数据 }   //被用来渲染页面
-    },
-    components: { HailaHeader, },
-    mounted() { this.getList(); },
-    watch: { '$route' (val) { this.getList(); } },
-    methods: {
-      getList() {
-        axios.get(URL, 参数).then((res) => { 业务处理 });
-        this.$router.push('cart');
-      }
-    },
-    filters: {
-      tofixed(value) { return value.toFixed(4) * 100 + '%'; }
-    },
-    computed:{
-      checkAllFlag(){ }
-    },
-  }
-</script>
 
