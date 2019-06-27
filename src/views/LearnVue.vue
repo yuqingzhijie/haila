6f04966477b14c66986d7f5512c5f97f2d18c702
yuqingzhijie <2711676102@qq.com>
2019-06-27 16:28:06 +0800

    Update LearnVue.vue

--- src/views/LearnVue.vue
+++ src/views/LearnVue.vue
@@ -1,13 +1,13 @@
 <template>
   <div>
-    <your-component></your-component
+    <your-component></your-component> <!--使用组件component-->
   </div>
 </template>
 
 <script>
-import YourComponent from 'yourpath/YourComponent'
+import YourComponent from 'yourpath/YourComponent' // 引入组件component
 export default {
-  components: {
+  components: { // 声明组件component
     YourComponent
   },
   data () {
