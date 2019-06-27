87445d59f59c3d80aca30c887ac876e635af8479
yuqingzhijie <2711676102@qq.com>
2019-06-27 16:24:20 +0800

    Update LearnVue.vue

--- src/views/LearnVue.vue
+++ src/views/LearnVue.vue
@@ -1,1 +1,17 @@
-12
+<template>
+  <div>
+    <your-component></your-component
+  </div>
+</template>
+
+<script>
+import YourComponent from 'yourpath/YourComponent'
+export default {
+  components: {
+    YourComponent
+  },
+  data () {
+    return {}
+  }
+}
+</script>
